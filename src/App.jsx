e85fdad61cc4ae30daa4b94b162214541c1665b8
Rakanmaj <22210020@htu.edu.jo>
2026-01-19 19:05:26 +0300

    Implement home page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import WeatherSuggestions from "./components/WeatherSuggestions";
+import Features from "./components/Features";
 
 function App() {
+  const [selectedCar, setSelectedCar] = useState(null);
+
+  const cars = [
+    {
+      car_id: 1,
+      type: "Convertible",
+      name: "BMW Z4",
+      image: "/assets/Z4Img.png",
+      price: 299,
+    },
+    {
+      car_id: 2,
+      type: "SUV",
+      name: "BMW X5",
+      image: "/assets/X5Img.png",
+      price: 249,
+    },
+    {
+      car_id: 3,
+      type: "Sedan",
+      name: "BMW M5",
+      image: "/assets/M5.png",
+      price: 349,
+    },
+    {
+      car_id: 4,
+      type: "Coupe",
+      name: "BMW M4",
+      image: "/assets/M4_Convertible.png",
+      price: 329,
+    },
+  ];
+
+  const handleSelectCar = (car) => {
+    setSelectedCar(car);
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -11,6 +52,9 @@ function App() {
           element={
             <>
               <Navbar />
+              <Hero />
+              <WeatherSuggestions onSelectCar={handleSelectCar} cars={cars} />
+              <Features />
               <Footer />
             </>
           }
